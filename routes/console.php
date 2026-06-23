6ebbed843314bce538029af471c8ed5ecc710a4d
daridaniel <dari.daniel@moneta.ng>
2026-06-23 10:56:51 +0100

    update the missing. line

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,6 @@ use App\Modules\SecureDB\Models\SecureDbProject;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
